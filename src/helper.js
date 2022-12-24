6c58cf0dd704252f2f9c621cca17576a5d23961c
HarshKunte <harshkunte99@gmail.com>
2022-12-24 22:33:06 +0530

    delete bookmark helper added

--- src/helper.js
+++ src/helper.js
@@ -1,11 +1,20 @@
 const { v4: uuidv4 } = require('uuid');
 
-export async function createBookmark(data){
+export function createBookmark(data){
     
     const item = {id:uuidv4(), ...data}
 
     let storageData = JSON.parse(localStorage.getItem("bookmarks") || '{"bookmarks":[], "groups":[{"name":"others", "id":0}]}');
     storageData.bookmarks.push(item)
+    localStorage.setItem('bookmarks', JSON.stringify(storageData))
+
+    return storageData
+}
+export function deleteBookmark(itemId){
+    
+    let storageData = JSON.parse(localStorage.getItem("bookmarks") || '{"bookmarks":[], "groups":[{"name":"others", "id":0}]}');
+    storageData.bookmarks.splice(storageData.bookmarks.findIndex(bookmark => bookmark.id === itemId) , 1)
+    console.log(storageData);
     localStorage.setItem('bookmarks', JSON.stringify(storageData))
 
     return storageData
