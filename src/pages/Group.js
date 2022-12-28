6f0532959b6030e4ac3a4d5e5e589221afe1ef59
HarshKunte <harshkunte99@gmail.com>
2022-12-28 23:58:47 +0530

    Group bookmarks visible.

--- src/pages/Group.js
+++ src/pages/Group.js
@@ -1,18 +1,23 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Card from '../components/Card';
 import Context from '../context';
 function Group() {
     const {id} = useParams();
     const {bookmarks} = useContext(Context);
-    const {groupBookmarks, setGroupBookmarks} = useState([])
+    const [groupBookmarks, setGroupBookmarks] = useState([])
 
     useEffect(()=>{
         let filteredBookmarks = bookmarks.bookmarks?.filter((item)=> item.group == id)
-        console.log(filteredBookmarks);
+        setGroupBookmarks(filteredBookmarks);
     },[])
-    return ( <div>
-        
-    </div> );
+    return ( <div className='flex flex-wrap py-10 justify-evenly sm:justify-evenly lg:justify-start'>
+    {
+        groupBookmarks?.map((data)=>(
+            <Card key={data.id} item={data}/>
+        ))
+    }
+</div> );
 }
 
 export default Group;
