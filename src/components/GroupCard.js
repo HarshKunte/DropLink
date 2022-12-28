518e474683e2311bc68e593165889bc1f88b82fc
HarshKunte <harshkunte99@gmail.com>
2022-12-28 23:54:21 +0530

    used Routes for redirecting to Group page

--- src/components/GroupCard.js
+++ src/components/GroupCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 function GroupCard({item}) {
     return ( 
-        <div className='w-36 h-24 sm:w-40 sm:h-28 xl:w-60 xl:h-40 bg-bgDarkGreen rounded-2xl xl:rounded-3xl shadow-xl relative mr-3 mb-4 lg:mr-5'>
+        <Link to={`/group/${item.id}`}  className='w-36 h-24 sm:w-40 sm:h-28 xl:w-60 xl:h-40 bg-bgDarkGreen rounded-2xl xl:rounded-3xl shadow-xl relative mr-3 mb-4 lg:mr-5'>
             <div className='w-full rounded-2xl xl:rounded-3xl h-[90%] xl:h-36 absolute bottom-0 bg-bgLightGreen p-4 lg:p-5'>
                 <p className='text-bgDarkGreen text-base sm:text-lg xl:text-2xl font-bold'>{item.name}</p>
                 <p className='text-bgDarkGreen text-xs xl:text-sm font-semibold'>{item.noOfBookmarks} bookmarks</p>
             </div>
-        </div>
+        </Link>
      );
 
 }
--- src/pages/Group.js
+++ src/pages/Group.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Context from '../context';
 function Group() {
+    const {id} = useParams();
+    const {bookmarks} = useContext(Context);
+    const {groupBookmarks, setGroupBookmarks} = useState([])
+
+    useEffect(()=>{
+        let filteredBookmarks = bookmarks.bookmarks?.filter((item)=> item.group == id)
+        console.log(filteredBookmarks);
+    },[])
     return ( <div>
-        Hello
+        
     </div> );
 }
 
